493a761ff0e2e878552625c5872402020a0aee8d
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-03-13 19:55:41 -0500

    feat(users): set the domain layer and user model
    Introduced the domain layer which encapsulates the initial business logic. In addition, the User model was configured with the initial fields and integrated into the Django administration interface.

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,1 +1,41 @@
 from django.contrib import admin
+from .models import User
+
+
+class UserAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the User model.
+
+    The admin panel will display the following fields: id, dni, full_name, email,
+    phone_number, password, is_staff, is_superuser, is_active, date_joined,
+    last_login.
+
+    The admin panel will also allow searching by the following fields: id, dni,
+    email, is_staff, is_superuser, is_active, date_joined.
+    """
+
+    list_display = (
+        "id",
+        "dni",
+        "full_name",
+        "email",
+        "phone_number",
+        "password",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "date_joined",
+        "last_login",
+    )
+    search_fields = (
+        "id",
+        "dni",
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "date_joined",
+    )
+
+
+admin.site.register(User, UserAdminPanel)
--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,1 +1,169 @@
+from django.contrib.auth.models import UserManager as BaseManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+from uuid import uuid4, UUID
+
+
+class UserManager(BaseManager):
+    """
+    Manager for the user model, provides methods to create user and superuser
+    instances.
+    """
+
+    def _create_user(
+        self,
+        id: UUID = None,
+        dni: str = None,
+        full_name: str = None,
+        email: str = None,
+        phone_number: str = None,
+        password: str = None,
+        **extra_fields,
+    ):
+        """
+        Create and save a User with the given attributes.
+        """
+
+        user = self.model(
+            id=id,
+            dni=dni,
+            full_name=full_name,
+            email=self.normalize_email(email),
+            phone_number=phone_number,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(
+        self,
+        id: UUID = None,
+        dni: str = None,
+        full_name: str = None,
+        email: str = None,
+        phone_number: str = None,
+        password: str = None,
+        **extra_fields,
+    ):
+        """
+        Create and save a User with the given email and password.
+        """
+
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", False)
+
+        return self._create_user(
+            id=id,
+            dni=dni,
+            full_name=full_name,
+            email=email,
+            phone_number=phone_number,
+            password=password,
+            **extra_fields,
+        )
+
+    def create_superuser(
+        self,
+        id: UUID = None,
+        dni: str = None,
+        full_name: str = None,
+        email: str = None,
+        phone_number: str = None,
+        password: str = None,
+        **extra_fields,
+    ):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        elif extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        elif extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True.")
+
+        return self._create_user(
+            id=id,
+            dni=dni,
+            full_name=full_name,
+            email=email,
+            phone_number=phone_number,
+            password=password,
+            **extra_fields,
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    This model represents a user in the system.
+    """
+
+    id = models.UUIDField(db_column="id", default=uuid4, primary_key=True)
+    dni = models.CharField(
+        db_column="dni",
+        max_length=8,
+        unique=True,
+        db_index=True,
+        null=True,
+        blank=True,
+    )
+    full_name = models.CharField(
+        db_column="full_name",
+        max_length=100,
+        unique=True,
+        null=True,
+        blank=True,
+    )
+    email = models.EmailField(
+        db_column="email",
+        max_length=100,
+        unique=True,
+        db_index=True,
+        null=False,
+        blank=False,
+    )
+    phone_number = PhoneNumberField(
+        db_column="phone_number", unique=True, null=True, blank=True
+    )
+    password = models.CharField(
+        db_column="password", max_length=128, null=False, blank=False
+    )
+    is_staff = models.BooleanField(
+        db_column="is_staff", default=False, serialize=False, db_index=True
+    )
+    is_superuser = models.BooleanField(
+        db_column="is_superuser", default=False, serialize=False, db_index=True
+    )
+    is_active = models.BooleanField(
+        db_column="is_active", default=False, db_index=True
+    )
+    date_joined = models.DateTimeField(
+        db_column="date_joined",
+        auto_now_add=True,
+        serialize=False,
+        db_index=True,
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name"]
+
+    class Meta:
+        db_table = "users"
+        verbose_name = "user"
+        verbose_name_plural = "users"
+        ordering = ["is_active", "-date_joined"]
+
+    def __str__(self):
+        return self.email
