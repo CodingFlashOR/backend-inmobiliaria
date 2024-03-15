c65c256c380ac9e5aca43039224e00903bffeb01
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-03-15 17:10:28 -0500

    docs(users): add user registration endpoint documentation
    This commit includes the documentation for the user registration endpoint. The documentation schema was generated using the drf-spectacular library.

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -17,9 +17,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.users.infrastructure.urls")),
+    path(
+        route="api/v1/doc/schema/",
+        view=SpectacularAPIView.as_view(),
+        name="schema",
+    ),
+    path(
+        route="api/v1/doc/swagger-ui/",
+        view=SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
