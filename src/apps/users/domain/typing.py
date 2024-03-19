6b667d2c1853ea5fb73f506c293fbe0f3587bc97
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-03-18 20:06:50 -0500

    feat(users): domain layer modification
    Added the interface for the repository that will handle queries to the database of the new JWT models where the tokens of the users that will be created will be stored and JWTBlacklisted to invalidate the tokens.

--- src/apps/users/domain/typing.py
+++ src/apps/users/domain/typing.py
@@ -1,5 +1,23 @@
-from typing import NewType
+from typing import NewType, Dict, Any
 
 
 UserUUID = NewType("UserUUID", str)
 UserUUID.__doc__ = "A unique identifier for a user."
+
+
+JWTType = NewType("JWTType", str)
+JWTType.__doc__ = "A JSON Web Token."
+
+
+AccessToken = NewType("AccessToken", str)
+AccessToken.__doc__ = "A type JSON Web Token used for authentication."
+
+
+RefreshToken = NewType("RefreshToken", str)
+RefreshToken.__doc__ = (
+    "A type JSON Web Token used for acquiring a new access and refresh token."
+)
+
+
+JWTPayload = NewType("JWTPayload", Dict[str, Any])
+JWTPayload.__doc__ = "A dictionary containing the data of a JSON Web Token."
