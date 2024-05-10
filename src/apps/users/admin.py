bd080c785b2a698e9857fe9d8324fcd8e1557941
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-05-10 13:05:50 -0500

    refactor(users-models): the database architecture was modified
    This new, more flexible architecture will allow more roles for users to be added more easily. The repositories of the modified models were adjusted.

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,41 +1,40 @@
 from django.contrib import admin
-from .models import User, JWT, JWTBlacklisted
+from .models import User, SearcherUser, JWT, JWTBlacklist
 
 
 class UserAdminPanel(admin.ModelAdmin):
     """
     Admin panel configuration for the User model.
-
-    The admin panel will display the following fields: id, dni, full_name, email,
-    phone_number, password, is_staff, is_superuser, is_active, date_joined,
-    last_login.
-
-    The admin panel will also allow searching by the following fields: id, dni,
-    email, is_staff, is_superuser, is_active, date_joined.
     """
 
-    list_display = (
-        "id",
-        "dni",
+    list_display = [
+        "uuid",
         "full_name",
         "email",
-        "phone_number",
         "password",
+        "profile",
+        "content_type",
         "is_staff",
         "is_superuser",
         "is_active",
         "date_joined",
         "last_login",
-    )
-    search_fields = (
-        "id",
-        "dni",
+    ]
+    search_fields = [
+        "uuid",
         "email",
-        "is_staff",
-        "is_superuser",
+        "profile",
         "is_active",
-        "date_joined",
-    )
+    ]
+
+
+class SearcherUserAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the SearcherUser model.
+    """
+
+    list_display = ["uuid", "address", "phone_number"]
+    search_fields = ["uuid"]
 
 
 class JWTAdminPanel(admin.ModelAdmin):
@@ -43,8 +42,15 @@ class JWTAdminPanel(admin.ModelAdmin):
     Admin panel configuration for the JWT model.
     """
 
-    list_display = ("id", "user", "jti", "token", "date_joined", "expires_at")
-    search_fields = ("user", "jti")
+    list_display = [
+        "uuid",
+        "user",
+        "jti",
+        "token",
+        "date_joined",
+        "expires_at",
+    ]
+    search_fields = ["user", "jti"]
 
 
 class JWTBlacklistedAdminPanel(admin.ModelAdmin):
@@ -52,10 +58,11 @@ class JWTBlacklistedAdminPanel(admin.ModelAdmin):
     Admin panel configuration for the JWTBlacklisted model.
     """
 
-    list_display = ("id", "token", "date_joined")
-    search_fields = ("token",)
+    list_display = ["uuid", "token", "date_joined"]
+    search_fields = ["token"]
 
 
 admin.site.register(User, UserAdminPanel)
+admin.site.register(SearcherUser, SearcherUserAdminPanel)
 admin.site.register(JWT, JWTAdminPanel)
-admin.site.register(JWTBlacklisted, JWTBlacklistedAdminPanel)
+admin.site.register(JWTBlacklist, JWTBlacklistedAdminPanel)
