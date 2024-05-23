80008137ad95aff7fedc1499f2f7dc054cd9d445
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-05-22 19:47:45 -0500

    chore(users): the serializer used for registering searcher users was changed
    This change is due to the fact that according to the requirements, only the base information of a user should be requested and not all of their information.

--- src/apps/users/infrastructure/serializers/__init__.py
+++ src/apps/users/infrastructure/serializers/__init__.py
@@ -1,4 +1,8 @@
-from .searcher_user import SearcherUserSerializer
+from .base import BaseUserSerializer
+from .searcher_user import (
+    SearcherUserSerializer,
+    SearcherUserRegisterSerializer,
+)
 from .jwt import (
     AuthenticationSerializer,
     TokenObtainPairSerializer,
