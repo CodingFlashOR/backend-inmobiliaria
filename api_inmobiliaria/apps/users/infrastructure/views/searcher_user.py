2e6748e62ad25feba0d9c196ad92efb6f3937721
Carlos Andres Aguirre Ariza <aguizandre.2803@gmail.com>
2024-07-29 19:57:03 -0500

    feat(users-views): view in charge of the registration logic of a search engine user adjusted

--- api_inmobiliaria/apps/users/infrastructure/views/searcher_user.py
+++ api_inmobiliaria/apps/users/infrastructure/views/searcher_user.py
@@ -2,7 +2,7 @@ from apps.users.infrastructure.serializers import (
     SearcherRegisterSerializer,
 )
 from apps.users.infrastructure.db import UserRepository
-from apps.users.infrastructure.views.base import MappedAPIView
+from apps.users.infrastructure.views.utils import MethodHTTPMapped
 from apps.users.infrastructure.schemas.searcher_user import (
     SearcherUserRegisterMethodSchema,
 )
@@ -10,13 +10,13 @@ from apps.users.applications import SearcherUsesCases
 from rest_framework.serializers import Serializer
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework import status
+from rest_framework import status, generics
 from typing import Dict, Any, List
 
 
-class SearcherUserAPIView(MappedAPIView):
+class SearcherUserAPIView(MethodHTTPMapped, generics.GenericAPIView):
     """
-    API view for managing operations for users with `searcheruser role`.
+    API view for managing operations for users with `searcher role`.
 
     It uses a mapping approach to determine the appropriate application logic,
     permissions, and serializers based on the HTTP method of the incoming request.
@@ -35,6 +35,9 @@ class SearcherUserAPIView(MappedAPIView):
     serializer_mapping = {
         "POST": SearcherRegisterSerializer,
     }
+    status_code_mapping = {
+        "POST": status.HTTP_201_CREATED,
+    }
 
     def _handle_valid_request(
         self, data: Dict[str, Any], request: Request
@@ -42,7 +45,7 @@ class SearcherUserAPIView(MappedAPIView):
         application = self.get_application_class()
         application(data=data, request=request)
 
-        return Response(status=status.HTTP_201_CREATED)
+        return Response(status=self.get_status_code())
 
     @staticmethod
     def _handle_invalid_request(errors: List[Dict[str, Any]]) -> Response:
@@ -59,12 +62,13 @@ class SearcherUserAPIView(MappedAPIView):
     @SearcherUserRegisterMethodSchema
     def post(self, request: Request, *args, **kwargs) -> Response:
         """
-        Handle POST requests for searcheruser registration.
+        Handle POST requests for searcher user registration.
 
-        This method allows the registration of a new seacher user, waiting for a POST
-        request with the registration data. A successful registration will consist of
-        saving the user's information in the database and sending a message to the
-        user's email with a link that will allow them to activate their account.
+        This method allows the registration of a new seacher user, waiting for a
+        POST request with the registration data. A successful registration will
+        consist of saving the user's information in the database and sending a
+        message to the user's email with a link that will allow them to activate
+        their account.
         """
 
         serializer_class = self.get_serializer_class()
