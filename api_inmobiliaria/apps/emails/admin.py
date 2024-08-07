7d9c12092c9d1291f912c7dc991ea934042b06eb
Carlos Andres Aguirre Ariza <aguizandre.2803@gmail.com>
2024-08-06 22:14:33 -0500

    feat(emails): Token model added to administration panel

--- api_inmobiliaria/apps/emails/admin.py
+++ api_inmobiliaria/apps/emails/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Token
 
-# Register your models here.
+
+@admin.register(Token)
+class TokenAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the Token model.
+    """
+
+    list_display = ["id", "token", "date_joined"]
+    search_fields = ["token"]
+    readonly_fields = ["date_joined", "id"]
+    ordering = ["-date_joined"]
