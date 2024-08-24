7261f6adfe44603047473f0be207ebc6ebd7233d
Carlos Andres Aguirre Ariza <aguizandre.2803@gmail.com>
2024-08-24 14:31:33 -0500

    feat(users): use case in charge of updating the searcher engine data implemented

--- api_inmobiliaria/apps/users/applications/data_manager.py
+++ api_inmobiliaria/apps/users/applications/data_manager.py
@@ -8,6 +8,10 @@ from django.db.models import Model
 class UserDataManager:
     """
     Manages user data operations.
+
+    This class interacts with a user repository to perform operations on user data. It
+    ensures that the user has the necessary permissions before performing any
+    operations.
     """
 
     def __init__(self, user_repository: IUserRepository) -> None:
@@ -31,4 +35,27 @@ class UserDataManager:
         ):
             raise PermissionDeniedAPIError()
 
-        return self._user_repository.get_role_data(user_base=user_base).first()
+        return self._user_repository.get_role_data(user_base=user_base)
+
+    def update(self, user_base: User, data: dict) -> Model:
+        """
+        Update the role data of a user.
+
+        #### Parameters:
+        - user_base: An instance of the User model.
+        - data: The data to update.
+
+        #### Raises:
+        - PermissionDeniedAPIError: If the user does not have the required permissions.
+        """
+
+        role_user = user_base.content_type.model
+
+        if not user_base.has_perm(
+            perm=USER_ROLE_PERMISSIONS[role_user]["change_data"]
+        ):
+            raise PermissionDeniedAPIError()
+
+        return self._user_repository.update_role_data(
+            user_base=user_base, data=data
+        )
