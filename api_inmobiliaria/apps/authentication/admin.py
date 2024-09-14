216591c0ad1648ec95a78c168be7077f76b16190
Carlos Andres Aguirre Ariza <aguizandre.2803@gmail.com>
2024-09-14 14:49:32 -0500

    feat(authentication): added JWT authentication method models

--- api_inmobiliaria/apps/authentication/admin.py
+++ api_inmobiliaria/apps/authentication/admin.py
@@ -1,3 +1,33 @@
+from apps.authentication.models import JWT, JWTBlacklist
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(JWT)
+class JWTAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the JWT model.
+    """
+
+    list_display = [
+        "id",
+        "user",
+        "jti",
+        "token",
+        "expires_at",
+        "date_joined",
+    ]
+    search_fields = ["user", "jti", "token"]
+    readonly_fields = ["date_joined"]
+    ordering = ["-date_joined"]
+
+
+@admin.register(JWTBlacklist)
+class JWTBlacklistedAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the JWTBlacklisted model.
+    """
+
+    list_display = ["id", "token", "date_joined"]
+    search_fields = ["token"]
+    readonly_fields = ["date_joined"]
+    ordering = ["-date_joined"]
--- api_inmobiliaria/apps/users/admin.py
+++ api_inmobiliaria/apps/users/admin.py
@@ -3,7 +3,7 @@ from .models import BaseUser, Searcher
 
 
 @admin.register(BaseUser)
-class UserAdminPanel(admin.ModelAdmin):
+class BaseUserAdminPanel(admin.ModelAdmin):
     """
     Admin panel configuration for the BaseUser model.
     """
@@ -45,36 +45,3 @@ class SearcherAdminPanel(admin.ModelAdmin):
     ]
     search_fields = ["uuid", "cc", "phone_number"]
     list_filter = ["is_phone_verified"]
-
-
-'''class JWTAdminPanel(admin.ModelAdmin):
-    """
-    Admin panel configuration for the JWT model.
-    """
-
-    list_display = [
-        "uuid",
-        "user",
-        "jti",
-        "token",
-        "date_joined",
-        "expires_at",
-    ]
-    search_fields = ["user", "jti", "token"]
-    readonly_fields = ["date_joined"]
-    ordering = ["-date_joined"]
-
-
-class JWTBlacklistedAdminPanel(admin.ModelAdmin):
-    """
-    Admin panel configuration for the JWTBlacklisted model.
-    """
-
-    list_display = ["uuid", "token", "date_joined"]
-    search_fields = ["token"]
-    readonly_fields = ["date_joined"]
-    ordering = ["-date_joined"]
-
-
-admin.site.register(JWT, JWTAdminPanel)
-admin.site.register(JWTBlacklist, JWTBlacklistedAdminPanel)'''
